91924781e7ef2f6c5805f15ac66e0b80716e72fe
Huginn-Muninn <43746764+Huginn-Muninn@users.noreply.github.com>
2020-04-05 22:47:16 +0000

    Add files via upload

---
+++ Scripts/pickcheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class pickcheck : MonoBehaviour
+{
+    public int target;
+    private int count;//fyrir teljarann
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Pickupp")
+        {
+            Destroy(collision.gameObject);
+            count = count + 1;
+            Debug.Log(count);
+        }
+    }
+    void Update()
+    {
+        if (count >= 5)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);//Leikurinn breytir um borð
+        }
+    }
+}
